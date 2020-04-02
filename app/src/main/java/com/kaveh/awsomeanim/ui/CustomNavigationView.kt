0e7861d4858cd240a8cb723105cbfc90728f906e
kaveh <kaveh.fazaeli@gmail.com>
2020-04-02 22:01:22 +0430

    pointer animation with line effect added

--- app/src/main/java/com/kaveh/awsomeanim/ui/CustomNavigationView.kt
+++ app/src/main/java/com/kaveh/awsomeanim/ui/CustomNavigationView.kt
@@ -1,5 +1,6 @@
 package com.kaveh.awsomeanim.ui
 
+import android.animation.Animator
 import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
 import android.content.Context
@@ -18,15 +19,19 @@ class CustomNavigationView @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : BottomNavigationView(context, attrs, defStyleAttr) {
     private val mPaint = Paint()
+    private val mLinePaint = Paint()
     private lateinit var mPoint: Pair<Float, Float>
+    private lateinit var mOldPoint: Pair<Float, Float>
     private lateinit var mListener: OnNavigationItemSelectedListener
     private var isListenerBound = false
+    private var isLineAnimationRun = false
+    private var mIndicatorRadius = 10F
     private var mIndicatorColor = Color.parseColor("#03DAC5")
 
     init {
         this.setOnNavigationItemSelectedListener { item ->
             if (::mListener.isInitialized) mListener.onNavigationItemSelected(item)
-            moveIndicatorX((2 * findSelectedItem(item.itemId) + 1) * (width / (this.menu.size() * 2F)))
+            moveIndicatorXType2((2 * findSelectedItem(item.itemId) + 1) * (width / (this.menu.size() * 2F)))
             true
         }
         isListenerBound = true
@@ -35,11 +40,17 @@ class CustomNavigationView @JvmOverloads constructor(
         mPaint.strokeCap = Paint.Cap.ROUND
         mPaint.isAntiAlias = true
         mPaint.color = mIndicatorColor
+        mLinePaint.style = Paint.Style.FILL
+        mLinePaint.strokeCap = Paint.Cap.ROUND
+        mLinePaint.isAntiAlias = true
+        mLinePaint.strokeWidth = mIndicatorRadius
+        mLinePaint.color = mIndicatorColor
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
         mPoint = Pair((2 * findSelectedItem(this.selectedItemId) + 1) * (width / (this.menu.size() * 2F)), height * .7F)
+        mOldPoint = mPoint
     }
 
     private fun moveIndicatorX(location: Float) {
@@ -50,11 +61,62 @@ class CustomNavigationView @JvmOverloads constructor(
         clickEffectAnim.start()
     }
 
+    private fun moveIndicatorXType2(location: Float) {
+        val clickEffectAnim = ObjectAnimator.ofFloat(
+                this, "indicatorLocationX", mPoint.first, location)
+        clickEffectAnim.duration = 150
+        clickEffectAnim.interpolator = AccelerateInterpolator()
+        clickEffectAnim.start()
+        val clickEffectAnim2 = ObjectAnimator.ofFloat(
+                this, "indicatorOldLocationX", mOldPoint.first, location)
+        clickEffectAnim2.duration = 150
+        clickEffectAnim2.interpolator = AccelerateInterpolator()
+        isLineAnimationRun = true
+        clickEffectAnim.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(animation: Animator?) {
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                clickEffectAnim2.start()
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+            }
+        })
+        clickEffectAnim2.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(animation: Animator?) {
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                isLineAnimationRun = false
+                mOldPoint = mPoint
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+            }
+        })
+    }
+
     var indicatorLocationX: Float
         get() = mPoint.first
         set(location) {
             if (mPoint.first != location) {
                 mPoint = Pair(location, mPoint.second)
+                invalidate()
+            }
+        }
+
+    var indicatorOldLocationX: Float
+        get() = mOldPoint.first
+        set(location) {
+            if (mOldPoint.first != location) {
+                mOldPoint = Pair(location, mOldPoint.second)
                 invalidate()
             }
         }
@@ -79,26 +141,10 @@ class CustomNavigationView @JvmOverloads constructor(
     }
 
     override fun onDraw(canvas: Canvas) {
-        canvas.drawCircle(mPoint.first, mPoint.second, 7F, mPaint)
+        if (isLineAnimationRun) {
+            canvas.drawLine(mOldPoint.first, mOldPoint.second, mPoint.first, mPoint.second, mLinePaint)
+        } else {
+            canvas.drawCircle(mPoint.first, mPoint.second, mIndicatorRadius, mPaint)
+        }
     }
-
-//    fun transform(fab: FloatingActionButton) {
-//        if (fab.isVisible) {
-//            fab.hide(object : FloatingActionButton.OnVisibilityChangedListener() {
-//                override fun onHidden(fab: FloatingActionButton?) {
-//                    super.onHidden(fab)
-//                    ValueAnimator.ofFloat(materialShapeDrawable.interpolation, 0F).apply {
-//                        addUpdateListener { materialShapeDrawable.interpolation = it.animatedValue as Float }
-//                        start()
-//                    }
-//                }
-//            })
-//        } else {
-//            ValueAnimator.ofFloat(materialShapeDrawable.interpolation, 1F).apply {
-//                addUpdateListener { materialShapeDrawable.interpolation = it.animatedValue as Float }
-//                doOnEnd { fab.show() }
-//                start()
-//            }
-//        }
-//    }
 }
